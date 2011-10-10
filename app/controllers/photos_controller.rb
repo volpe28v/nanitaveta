afa59632b3119cc8c51ab4bb1618ac7968567e85
naoki kodama <naoki.koda@gmail.com>
2011-10-10 12:06:17 +0900

    users#show から photos#index に写真リストを移動 認証周りの画面遷移を調整.ログイン画面が最初にくるようにした。

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,35 @@
 class PhotosController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
   end
 
   def show
+    @photo = Photo.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 
   def destroy
+    @photo = Photo.find(params[:id])
+    @user = User.find(params[:user_id])
+
+    @photo.destroy
+    File.delete(Rails.root + "public/photos/" + @photo.path)
+
+    redirect_to @user
   end
 
   def edit
+    @photo = Photo.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 
   def update
+    photo = Photo.find(params[:id])
+    user = User.find(params[:user_id])
+
+    photo.update_attributes(params[:photo])
+
+    redirect_to user_photo_path(user,photo)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if current_user
+      redirect_to user_photos_path(current_user)
+    elsif
+      redirect_to new_user_session_path
+    end
   end
 
   def show
