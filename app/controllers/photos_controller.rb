b5cbe0aac50657d6fd13f336afd90e53275565bf
naoki kodama <naoki.koda@gmail.com>
2011-10-15 16:02:52 +0900

    写真ページには認証を必要にした

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_filter :authenticate_user! 
+
   def index
     @user = User.find(params[:user_id])
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user! 
+
   def index
     if current_user
       redirect_to user_photos_path(current_user)
