e376720b890662ccfe5b528a0718912a9967a4eb
naoki kodama <naoki.koda@gmail.com>
2011-10-16 15:08:51 +0900

    price計算で不要な分岐を削除

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -46,10 +46,7 @@ class Photo < ActiveRecord::Base
 
 
   def price
-    if /(\d+)円/ =~ self.message
-      $1 ? $1.to_i : 0
-    else
-      0
-    end
+    /(\d+)円/ =~ self.message
+    $1 ? $1.to_i : 0
   end
 end
