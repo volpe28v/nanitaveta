6198eaa5054a5e6721f097e5b1c9af58d1399d75
naoki kodama <naoki.koda@gmail.com>
2011-10-20 02:38:24 +0900

    見栄え変更。 ローディング画像にフェード処理

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,13 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 function sync_now( url, to_path ){
-  $("#sync_auto_loader").show();
+  $("#sync_auto_loader").fadeIn();
   $.ajax({
      type: "GET",
      cache: false,
      url: url,
      success: function(result){
-       $("#sync_auto_loader").hide();
+       $("#sync_auto_loader").fadeOut();
        if (result == "OK"){
          location.href = to_path;
        }
